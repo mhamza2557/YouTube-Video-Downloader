872390c0e7362658945af76afbebd20ae147bd14
Hamza <mhamza2551@gmail.com>
2020-05-08 18:21:18 +0500

    Update YouTubeVideoDownloader.py

--- YouTubeVideoDownloader.py
+++ YouTubeVideoDownloader.py
@@ -4,15 +4,15 @@ from pytube.cli import on_progress
 
 url = input('Enter YouTube URL Here: ')
 yt1 = YouTube(url, on_progress_callback=on_progress)
-getResolution = yt1.streams.filter(adaptive=True, mime_type="video/mp4")
+getResolution = yt1.streams.filter(adaptive=True)
 
 count = 0
-for i in yt1.streams.filter(adaptive=True, mime_type="video/mp4"):
-    fileSize = (yt1.streams.filter(adaptive=True, mime_type="video/mp4", res=getResolution[count].resolution).first().filesize) / (1024 * 1024)
-    print(str(count) + ' ) ' + str(i.resolution) + ' - ' + str(i.mime_type) + ' - Approx: ' + str(float("{:.2f}".format(fileSize))) + ' MBs' )
+for i in yt1.streams.filter(adaptive=True):
+    fileSize = (yt1.streams.filter(adaptive=True, res=getResolution[int(count)].resolution).first().filesize) / (1024 * 1024)
+    print(str(count) + ' ) ' + str(i.resolution) + ' - ' + str(i.mime_type) + ' - ' + str(float("{:.2f}".format(fileSize))) + ' MBs' )
     count = count + 1
 
 inputResolution = input('\nEnter number # for resolution: ')
-print(yt1.streams.filter(adaptive=True, mime_type="video/mp4", res=getResolution[int(inputResolution)].resolution).first())
-video = yt1.streams.filter(adaptive=True, mime_type="video/mp4", res=getResolution[int(inputResolution)].resolution).first()
+print(yt1.streams.filter(adaptive=True, res=getResolution[int(inputResolution)].resolution).first())
+video = yt1.streams.filter(adaptive=True, res=getResolution[int(inputResolution)].resolution).first()
 video.download()
